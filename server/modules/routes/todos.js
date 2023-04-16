60a6701aadc64152225e49394574df203c6203c3
Kobi Jones <kobijonesmail@gmail.com>
2023-04-16 12:48:45 -0500

    Add delete to dummy data interface

--- server/modules/routes/todos.js
+++ server/modules/routes/todos.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { addTodo, getTodos } from '../data/database.js'
+import { addTodo, deleteTodo, getTodos } from '../data/dummy.js'
 
 const router = Router()
 
@@ -18,6 +18,25 @@ router.post('/', (req, res) => {
   addTodo(req.body)
     .then(() => {
       res.sendStatus(201)
+    })
+    .catch(err => {
+      console.error(err)
+      res.sendStatus(500)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  if (!Number.isInteger(id)) {
+    console.error('id is not an integer')
+    res.sendStatus(500)
+    return
+  }
+
+  deleteTodo(id)
+    .then(() => {
+      res.sendStatus(200)
     })
     .catch(err => {
       console.error(err)
